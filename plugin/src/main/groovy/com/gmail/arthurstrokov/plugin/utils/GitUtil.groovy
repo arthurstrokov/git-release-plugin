1b1548d3ef2f1824c2aa21ac533d79e3868b9658
Arthur Strokov <arthurstrokov@gmail.com>
2022-03-13 22:16:06 +0300

    up

--- plugin/src/main/groovy/com/gmail/arthurstrokov/plugin/utils/GitUtil.groovy
+++ plugin/src/main/groovy/com/gmail/arthurstrokov/plugin/utils/GitUtil.groovy
@@ -20,7 +20,6 @@ public class GitUtil {
 
     public static def getCurrentTagVersion() {
         def tags = getGitTagsResult
-        println(tags)
         if (tags.isEmpty()) {
             throw new GradleScriptException("There is no tags", null)
         }
