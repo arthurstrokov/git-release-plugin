71ecdef3dd8a641f8c7e250b6ab38ca5d817768f
Arthur Strokov <arthurstrokov@gmail.com>
2022-03-27 17:59:21 +0300

    add GitTagVersionTask.groovy

--- plugin/src/main/groovy/com/gmail/arthurstrokov/plugin/utils/GitUtil.groovy
+++ plugin/src/main/groovy/com/gmail/arthurstrokov/plugin/utils/GitUtil.groovy
@@ -14,26 +14,9 @@ class GitUtil {
         return ("git status").execute().text
     }
 
-    static def getCurrentBranch() {
-        return ("git branch --show-current").execute().text
+    static def getGitTags() {
+        return ("git tag -l").execute().text
     }
-
-    static def getCurrentTagVersion() {
-        def tags = ("git tag -l").execute().text
-        if (tags.isEmpty()) {
-            createTag("v0.0")
-        }
-        def tagsArray = tags.replace("v", "").split("\n")
-        def floatTagsArray = []
-        for (element in tagsArray) {
-            floatTagsArray.add(Float.parseFloat(element))
-        }
-        def floatTagsArraySorted = floatTagsArray.collect { it as Float }.sort { it } // or -it for reverse
-        def currentTagVersion = "v" + floatTagsArraySorted[floatTagsArraySorted.size() - 1]
-        return currentTagVersion
-    }
-
-    public static final def getTags = ("git tag").execute().text
 
     static def createTag(String version) {
         def result = ("git tag -a $version -m \"Created\"").execute().text
@@ -41,6 +24,12 @@ class GitUtil {
             throw new GradleScriptException("Tag was not created", null)
         }
     }
+
+    static def getCurrentBranch() {
+        return ("git branch --show-current").execute().text
+    }
+
+//    public static final def getTags = ("git tag").execute().text
 
     static def getLastCommitLog() {
         return ("git log -1").execute().text
