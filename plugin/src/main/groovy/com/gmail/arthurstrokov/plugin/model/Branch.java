c5026cca73e87eb10d185513b96b3379c8ce4718
Arthur Strokov <arthurstrokov@gmail.com>
2022-03-28 00:39:10 +0300

    change class name to PluginBranchModel.java

--- plugin/src/main/groovy/com/gmail/arthurstrokov/plugin/model/Branch.java
+++ plugin/src/main/groovy/com/gmail/arthurstrokov/plugin/model/Branch.java
@@ -2,16 +2,18 @@ package com.gmail.arthurstrokov.plugin.model;
 
 public class Branch {
 
-    private static String major = "master";
-
     private Branch() {
     }
+
+    private static String major = "master";
+
 
     public static String major() {
         return major;
     }
 
     public static void setMajor(String major) {
+
         Branch.major = major;
     }
 }
