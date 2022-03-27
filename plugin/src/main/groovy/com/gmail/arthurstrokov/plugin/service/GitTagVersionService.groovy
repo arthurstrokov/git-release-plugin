78747fd140f11bac18db4487c67b0c88ff2e74f8
Arthur Strokov <arthurstrokov@gmail.com>
2022-03-27 20:25:50 +0300

    up GitTagVersionService

--- plugin/src/main/groovy/com/gmail/arthurstrokov/plugin/service/GitTagVersionService.groovy
+++ plugin/src/main/groovy/com/gmail/arthurstrokov/plugin/service/GitTagVersionService.groovy
@@ -1,11 +1,14 @@
 package com.gmail.arthurstrokov.plugin.service
 
+import org.gradle.api.GradleScriptException
+
 class GitTagVersionService {
 
     static gitTagVersion() {
         def gitTags = GitCommandService.gitTags()
         if (gitTags.empty) {
             GitCommandService.addNewTag("v0.0")
+            throw new GradleScriptException("Tag list was empty. Tag 'v0.0' has been created", null)
         }
         def tagsArray = gitTags.replace("v", "").split("\n")
         def floatTagsArray = []
