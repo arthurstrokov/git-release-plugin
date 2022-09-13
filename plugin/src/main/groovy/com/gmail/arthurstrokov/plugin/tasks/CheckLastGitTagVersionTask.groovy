2da3893a394d019bd0f189634020bb9c645020cf
Arthur <arthurstrokov@gmail.com>
2022-09-13 21:19:00 +0300

    ref

--- plugin/src/main/groovy/com/gmail/arthurstrokov/plugin/tasks/CheckLastGitTagVersionTask.groovy
+++ plugin/src/main/groovy/com/gmail/arthurstrokov/plugin/tasks/CheckLastGitTagVersionTask.groovy
@@ -2,13 +2,19 @@ package com.gmail.arthurstrokov.plugin.tasks
 
 import com.gmail.arthurstrokov.plugin.service.GitCommandService
 import org.gradle.api.DefaultTask
+import org.gradle.api.GradleScriptException
 import org.gradle.api.tasks.TaskAction
 
 class CheckLastGitTagVersionTask extends DefaultTask {
 
     @TaskAction
     def checkLastGitTagVersion() {
-        def lastGitTagVersion = GitCommandService.lastGitTag()
-        println(lastGitTagVersion)
+        def gitTags = GitCommandService.getGitTags()
+        if (gitTags.empty) {
+            throw new GradleScriptException("Tag list is empty. Create tag first. Example: 'v0.0'", null)
+        } else {
+            def lastGitTagVersion = GitCommandService.lastGitTag()
+            println(lastGitTagVersion)
+        }
     }
 }
